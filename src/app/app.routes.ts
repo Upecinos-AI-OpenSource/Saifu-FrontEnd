12f71b8f90c15e16607925f0e31441e400b8c594
Andres2580 <143357014+Andres2580@users.noreply.github.com>
2024-09-25 23:45:01 -0500

    feat: added components login and signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {SignupComponent} from "./authentication/components/signup/signup.component";
+import {LoginComponent} from "./authentication/components/login/login.component";
+import {NgModule} from "@angular/core";
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: '',redirectTo: 'login', pathMatch: 'full'},
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ src/app/authentication/components/login/login.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatCard, MatCardActions, MatCardContent, MatCardTitle} from "@angular/material/card";
+import {MatFormField} from "@angular/material/form-field";
+import {MatIcon} from "@angular/material/icon";
+import {MatInput} from "@angular/material/input";
+import {Router, RouterLink} from "@angular/router";
+import {MatButton} from "@angular/material/button";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AuthService} from "../../../shared/services/auth.service";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    MatToolbar,
+    MatCard,
+    MatCardContent,
+    MatCardTitle,
+    MatFormField,
+    MatIcon,
+    MatCardActions,
+    MatInput,
+    RouterLink,
+    MatButton,
+    ReactiveFormsModule
+  ],
+
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  loginForm;
+
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  get email() {
+    return this.loginForm.controls['email'];
+  }
+  get password() {
+    return this.loginForm.controls['password'];
+  }
+
+  loginUser(){
+    const {email, password} = this.loginForm.value;
+    this.authService.getUserByEmail(email as string).subscribe(
+      response => {
+        if (response.length > 0 && response[0].password === password) {
+          sessionStorage.setItem('email', email as string);
+          this.router.navigate(['/login']);
+          //this.router.navigate(['/home']);
+        } else {
+          alert('Invalid email or password');
+        }
+
+      }
+
+    )
+  }
+
+}
